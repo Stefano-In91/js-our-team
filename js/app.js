77d9f51cd6d0a5e80484102c894e422ba0f8d938
Stefano.In91 <ste.ince91@gmail.com>
2022-11-07 15:18:39 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const team = [member1, member2, member3, member4, member5, member6];
+
+const member1 = {
+  firstName: `Wayne`,
+  lastName: `Barnett`,
+  position: `Founder & CEO`,
+  thumbnail: `wayne-barnett-founder-ceo.jpg`,
+};
+const member2 = {
+  firstName: `Angela`,
+  lastName: `Caroll`,
+  position: `Chief Editor`,
+  thumbnail: `angela-caroll-chief-editor.jpg`,
+};
+const member3 = {
+  firstName: `Walter`,
+  lastName: `Gordon`,
+  position: `Office Manager`,
+  thumbnail: `walter-gordon-office-manager.jpg`,
+};
+const member4 = {
+  firstName: `Angela`,
+  lastName: `Lopez`,
+  position: `Social Media Manager`,
+  thumbnail: `angela-lopez-social-media-manager.jpg`,
+};
+const member5 = {
+  firstName: `Scott`,
+  lastName: `Estrada`,
+  position: `Developer`,
+  thumbnail: `scott-estrada-developer.jpg`,
+};
+const member6 = {
+  firstName: `Barbara`,
+  lastName: `Ramos`,
+  position: `Graphic Designer`,
+  thumbnail: `barbara-ramos-graphic-designer.jpg`,
+};
